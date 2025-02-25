b908ede616abbd4a7bdb7026953306914ac4ae70
Pedro Germano <pedrogermano232@gmail.com>
2025-02-25 10:38:53 -0300

    Props

--- 03-pizza-menu/final/src/index.js
+++ 03-pizza-menu/final/src/index.js
@@ -58,128 +58,65 @@ function App() {
 }
 
 function Header() {
-  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
-  const style = {};
-
   return (
     <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
+      <h1>Fast React Pizza Co.</h1>
     </header>
   );
 }
-
 function Menu() {
-  const pizzas = pizzaData;
-  // const pizzas = [];
-  const numPizzas = pizzas.length;
-
   return (
     <main className="menu">
       <h2>Our menu</h2>
-
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
-          </p>
-
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>We're still working on our menu. Please come back later :)</p>
-      )}
-
-      {/* <Pizza
+      <Pizza
         name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        ingredients="Tomato, mozarella, spinach, and cheese"
         photoName="pizzas/spinaci.jpg"
-        price={10}
+        price="10"
       />
       <Pizza
         name="Pizza Funghi"
         ingredients="Tomato, mushrooms"
-        price={12}
         photoName="pizzas/funghi.jpg"
-      /> */}
+        price="12"
+      />
     </main>
   );
 }
 
-function Pizza({ pizzaObj }) {
-  console.log(pizzaObj);
-
-  // if (pizzaObj.soldOut) return null;
-
+function Pizza(props) {
   return (
-    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
       <div>
-        <h3>{pizzaObj.name}</h3>
-        <p>{pizzaObj.ingredients}</p>
-
-        {/* {pizzaObj.soldOut ? (
-          <span>SOLD OUT</span>
-        ) : (
-          <span>{pizzaObj.price}</span>
-        )} */}
-
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
       </div>
-    </li>
-  );
-}
-
-function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  // else alert("Sorry we're closed");
-
-  // if (!isOpen) return <p>CLOSED</p>;
-
-  return (
-    <footer className="footer">
-      {isOpen ? (
-        <Order closeHour={closeHour} openHour={openHour} />
-      ) : (
-        <p>
-          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
-        </p>
-      )}
-    </footer>
-  );
-
-  // return React.createElement("footer", null, "We're currently open!");
-}
-
-function Order({ closeHour, openHour }) {
-  return (
-    <div className="order">
-      <p>
-        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
-        online.
-      </p>
-      <button className="btn">Order</button>
     </div>
   );
 }
 
-// React v18
+function Footer() {
+  // const hour = new Date().getHours();
+  // const openHour = 12;
+  // const closeHour = 22;
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed ");
+
+  return (
+    <>
+      <footer className="footer">
+        {new Date().toLocaleTimeString()} We're currently open
+      </footer>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// React before 18
-// ReactDOM.render(<App />, document.getElementById("root"));
