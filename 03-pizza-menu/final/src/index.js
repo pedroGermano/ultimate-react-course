a3801b6c0977a70af31e7646587996b6ab93fd09
Pedro Germano <pedrogermano232@gmail.com>
2025-02-27 11:08:02 -0300

    ok

--- 03-pizza-menu/final/src/index.js
+++ 03-pizza-menu/final/src/index.js
@@ -105,19 +105,23 @@ function Pizza(props) {
 }
 
 function Footer() {
-  // const hour = new Date().getHours();
-  // const openHour = 12;
-  // const closeHour = 22;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we're closed ");
 
   return (
-    <>
-      <footer className="footer">
-        {new Date().toLocaleTimeString()} We're currently open
-      </footer>
-    </>
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
   );
 }
 
